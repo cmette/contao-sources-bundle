ee3374b2a4bee4cdf2c13f8a3a0784e377708e7b
cmette <info@schreiber-mette.de>
2026-03-27 16:15:47 +0100

    fix missing controller

---
+++ src/Controller/ExampleController.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Contao Pedigree Bundle for Contao Open Source CMS
+ *
+ * Copyright (c) 2023 C. Mette
+ *
+ * @package    contao-pedigree-bundle
+ * @link       https://github.com/cmette/contao-pedigree-bundle
+ * @license    LGPL-3.0-or-later
+ * @author     Christian Mette
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cmette\ContaoSourcesBundle\Controller;
+
+use Contao\CoreBundle\Controller\AbstractBackendController;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[
+    Route(
+        '/%contao.backend.route_prefix%/example',
+        name: ExampleController::class,
+        defaults: [
+            '_scope' => 'backend',
+            "_token_check" => false
+        ]
+    )
+]
+class ExampleController extends AbstractBackendController
+{
+    public string $templateRoot = '@Contao_CmetteContaoPedigreeBundle/backend';
+    protected string $assetsDir = 'bundles/cmettecontaopedigree';
+    private Request $request;
+    // holds all POST parameters
+    private array $postParams = [];
+    // path to the widgets icons
+    protected string $resImg;
+
+    public function __construct()
+    {
+        #System::loadLanguageFile('tl_pedigree_image');
+        #System::loadLanguageFile('default');
+
+        #$this->resImg   = Environment::get('base')."$this->assetsDir/img/resumable";
+
+        #$this->request  = System::getContainer()->get('request_stack')->getCurrentRequest();
+
+        #parse_str($this->request->getContent(), $this->postParams);
+    }
+
+    #[Route('/scan', defaults: ['_scope' => 'backend'])]
+    public function scan(): Response
+    {
+
+        return new Response('OK', Response::HTTP_OK);
+    }
+}
